95d08fb379ae2bae2ef2876a3a41f6bee4210fce
Sergey <bs6609419@gmail.com>
2026-01-10 13:39:12 +0700

    Fix Spring Boot version to 3.2.0 and CORS config

--- src/main/java/com/sergey/myapp_backend/config/WebConfig.java
+++ src/main/java/com/sergey/myapp_backend/config/WebConfig.java
@@ -9,15 +9,18 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:8080",
                         "http://10.0.2.2:8080",      // Для эмулятора Android
                         "http://192.168.1.15:8080",  // Для телефона в локальной сети
-                        "http://192.168.1.*:8080"     // Для всех устройств в сети 192.168.1.x
+                        "http://192.168.1.*:8080",// Для всех устройств в сети 192.168.1.x
+                        "https://todo-app-backend-production-2171.up.railway.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(false);
+                .allowCredentials(true)
+                .maxAge(3600);
+
     }
 }
